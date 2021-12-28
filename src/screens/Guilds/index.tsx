37d8658260027354d8dc5e3a4c5651e40902266a
ojeffpinheiro01 <jefersonpinheirodesouza@gmail.com>
2021-12-28 20:38:06 -0300

    tela da guild

---
+++ src/screens/Guilds/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FlatList, Text, View } from "react-native";
+
+import { ListDivider } from '../../components/ListDivider';
+
+import { styles } from "./styles";
+
+export function Guilds(){
+  const guilds = [
+    {
+      id: '1',
+      name: 'Lendários',
+      icon: 'image.png',
+      owner: true
+    },
+    {
+      id: '2',
+      name: 'Galera do Game',
+      icon: 'image.png',
+      owner: true
+    }
+  ];
+
+  return(
+    <View style={styles.container}>
+      <FlatList data={guilds}
+        keyExtractor={item => item.id }
+        renderItem={({ item }) => (
+          <Text>{item.name}</Text>
+        )}
+        showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={() => <ListDivider />}
+        ListHeaderComponent={() => <ListDivider />}
+        contentContainerStyle={{ paddingBottom: 68, paddingTop: 103 }}
+        style={styles.guilds} />
+    </View>
+  )
+}
