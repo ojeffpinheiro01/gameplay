5c021b86da1d1e03e66c3171f6996902e4b551bf
ojeffpinheiro01 <jefersonpinheirodesouza@gmail.com>
2021-12-27 23:25:23 -0300

    fontes

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,14 +1,11 @@
 export const theme = {
   colors: {
-    background: '#0D133D',
-    
     primary: '#E51C44',
     
     secondary100: '#0A1033',
     secondary90: '#0D133D',    
     secondary80: '#0E1647',
-    secondary85: '#171F52',
-    secondary70: '#1B2565', 
+    secondary70: '#1B2565',
     secondary60: '#1B2565',
     secondary50: '#243189', 
     secondary40: '#1D2766', 
@@ -19,8 +16,6 @@ export const theme = {
     heading: '#DDE3F0',
     line: '#991F36',
     on: '#32BD50',
-
-    discord: '#7289da',
   },
 
   fonts: {
