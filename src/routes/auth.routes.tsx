fa8f877cae3a9881ebe9547548e2f56fbadec736
ojeffpinheiro01 <jefersonpinheirodesouza@gmail.com>
2021-12-28 15:54:07 -0300

    lista de guilds

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { Home } from '../screens/Home'
-import { SignIn } from '../screens/SignIn'
+import { Home } from '../screens/Home';
+import { SignIn } from '../screens/SignIn';
 
-const { Navigator, Screen } = createStackNavigator()
+const { Navigator, Screen } = createStackNavigator();
 
 export function AuthRoutes() {
   return (
-      <Navigator
-        screenOptions={{
-          headerShown: false,
-          cardStyle: {
-            backgroundColor: 'transparent'
-          }
-        }} >
-        <Screen name='SignIn' component={SignIn} />
-        <Screen name='Home' component={Home} />
-      </Navigator>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: 'transparent' }
+      }}>
+      <Screen name="Home" component={Home} />
+      <Screen name="SignIn" component={SignIn}/>
+    </Navigator>
   )
 }
